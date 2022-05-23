e332674c09385c525591ce7c1ce2018fdfa2e7a9
Ryszard-Penkala <ryszard.penkala@gmail.com>
2022-05-23 21:16:37 +0200

    Added searching and listing of AdRecords.

--- records/ad.record.ts
+++ records/ad.record.ts
@@ -1,4 +1,4 @@
-import {AdEntity, NewAdEntity} from "../types";
+import {AdEntity, NewAdEntity, SimpleAddEntity} from "../types";
 import {ValidationError} from "../utils/errors";
 import {pool} from "../utils/db";
 import {FieldPacket} from "mysql2";
@@ -45,12 +45,21 @@ export class AdRecord implements AdEntity {
         this.lon = obj.lon;
 
     }
-
     static async getOne(id: string): Promise<AdRecord | null>  {
-        const [results] = await pool.execute("SELECT * FROM `ads` WHERE id= :id", {
+        const [results] = await pool.execute("SELECT * FROM `ads` WHERE `id` = :id", {
             id,
         }) as AdRecordResults;
         return results.length === 0 ? null : new AdRecord(results[0]);
+    }
+
+    static async findAll(name: string | null): Promise<SimpleAddEntity[]> {
+        const [results] = await pool.execute("SELECT * from `ads` WHERE `name` LIKE :search", {
+            search: `%${name}%`,
+        }) as AdRecordResults;
+        return results.map(result => {
+            const {id, lat, lon} = result;
+            return {id, lat, lon}
+        });
     }
 
 }
--- tests/ad.test.ts
+++ tests/ad.test.ts
@@ -1,16 +1,51 @@
 import {AdRecord} from "../records/ad.record";
+import { pool } from "../utils/db";
+import {AdEntity} from "../types";
 
-test('AdRecord returns data from database for one entry', async () => {
+afterAll(async() => {
+    await pool.end();
+})
+
+test('AdRecord.getOne returns data from database for one entry', async () => {
     const ad = await AdRecord.getOne('abc');
 
     expect(ad).toBeDefined();
     expect(ad.id).toBe('abc');
     expect(ad.name).toBe('Testowa');
-})
+});
 
-test('AdRecord returns null for unexisting entry', async ()=>{
+test('AdRecord.getOne returns null for unexisting entry', async ()=>{
     const ad = await AdRecord.getOne('---');
 
     expect(ad).toBeNull();
-})
+});
+
+
+test('AdRecord.findAll returns array of found entries.', async ()=>{
+    const ads = await AdRecord.findAll('');
+
+    expect(ads).not.toEqual([] );
+    expect(ads[0].id).toBeDefined();
+
+});
+
+test('AdRecord.findAll returns array of found entries when searching for "a".', async ()=>{
+    const ads = await AdRecord.findAll('a');
+
+    expect(ads).not.toEqual([] );
+    expect(ads[0].id).toBeDefined();
+});
+
+test('AdRecord.findAll returns empty array when searching for something that is not existing.', async ()=>{
+    const ads = await AdRecord.findAll('-------------------');
+
+    expect(ads).toEqual([]);
+});
+
+test('AdRecord.findAll returns smaller amount of data.', async ()=>{
+    const ads = await AdRecord.findAll('');
+
+    expect((ads[0] as AdEntity).price).toBeUndefined()
+    expect((ads[0] as AdEntity).description).toBeUndefined();
+});
 
